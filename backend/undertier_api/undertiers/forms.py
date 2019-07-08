a1d4e6e93d95f0bb0a1a07d1c935324c5223f6a2
Joshua Cochran <joshuamcochran@gmail.com>
2019-07-08 04:41:49 -0700

    Fixed bug in forms.py accessing forms.ModelForm instead of ModelForm.

--- backend/undertier_api/undertiers/forms.py
+++ backend/undertier_api/undertiers/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Alliance
 
-class SynergiesAdminForm(forms.ModelForm):
+class SynergiesAdminForm(ModelForm):
     class Meta:
         model = Alliance
         fields = '__all__'
