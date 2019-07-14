63461ee373fd97433e5aa217c3e352b9a6075196
Joshua Cochran <joshuamcochran@gmail.com>
2019-07-14 15:10:26 -0700

    More changes to react-dnd tut to better understand the library.

--- frontend/src/components/MapTest/Board.js
+++ frontend/src/components/MapTest/Board.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react'
 import Square from './Square'
 import Knight from './Knight'
-import { moveKnight } from './Game'
-import { timingSafeEqual } from 'crypto';
-
-function handleSquareClick(toX, toY) {
-	moveKnight(toX, toY)
-}
-
-function renderSquare(i, [knightX, knightY]) {
-		const x = i % 8
-		const y = Math.floor(i / 8)
-		const black = (x + y) % 2 === 1
-		const isKnightHere = knightX === x && knightY === y
-		const piece = isKnightHere ? <Knight /> : null
-		
-		return (
-			<div key={i} style={{ width: '12.5%', height: '12.5%' }} onClick={() => handleSquareClick(x, y)}>
-				<Square black={black}>{piece}</Square>
-			</div>
-
-		)
-}
 
 class Board extends Component {
 	constructor(props) {
@@ -47,10 +26,28 @@ class Board extends Component {
 		this.updateSquares();
 	}
 
+	renderSquare(i, [knightX, knightY]) {
+		const x = i % 8
+		const y = Math.floor(i / 8)
+		const black = (x + y) % 2 === 1
+		const isKnightHere = knightX === x && knightY === y
+		const piece = isKnightHere ? <Knight /> : null
+		
+		return (
+			<div key={i} style={{ width: '12.5%', height: '12.5%' }} onClick={() => this.handleSquareClick(x, y)}>
+				<Square black={black}>{piece}</Square>
+			</div>
+		)
+	}
+
+	handleSquareClick(toX, toY) {
+		this.setState({knightPosition: [toX, toY]});
+	}
+
 	updateSquares = () => {
 		const squareData = []
 		for (let i = 0; i < 64; i++) {
-			squareData.push(renderSquare(i , this.state.knightPosition));
+			squareData.push(this.renderSquare(i , this.state.knightPosition));
 			this.setState({ squares: squareData });
 		}
 	}
